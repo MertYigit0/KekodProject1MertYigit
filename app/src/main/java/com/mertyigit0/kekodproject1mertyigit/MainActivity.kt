1cfffdd39a9fb2b6c6327da27b5628edcd28f085
merty <mertyigit148@gmail.com>
2024-08-26 16:51:50 +0300

    Dependencies and Fragments

--- app/src/main/java/com/mertyigit0/kekodproject1mertyigit/MainActivity.kt
+++ app/src/main/java/com/mertyigit0/kekodproject1mertyigit/MainActivity.kt
@@ -5,16 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.mertyigit0.kekodproject1mertyigit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // View Binding'i başlatıyoruz
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // View Binding ile ana layout'a erişim sağlıyoruz
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // NavController ve Bottom Navigation Bar'ı bağla
+        val navHostFragment = binding.fragmentContainerView.getFragment<NavHostFragment>()
+        val navController = navHostFragment?.findNavController()
+        if (navController != null) {
+            binding.bottomNavigationView.setupWithNavController(navController)
         }
     }
 }
